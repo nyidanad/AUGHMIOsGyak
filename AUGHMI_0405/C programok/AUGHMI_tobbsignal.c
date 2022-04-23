6f384ac21d2e0ecdf7c734d1dea5fea17f4aab24
NyDaniel <91250016+nyidanad@users.noreply.github.com>
2022-04-23 13:32:23 +0200

    Add files via upload

---
+++ AUGHMI_0405/C programok/AUGHMI_tobbsignal.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handlerSignals(int sig);
+
+int main()
+{
+    printf("PID = %d\n", getpid());
+
+    if(signal(SIGINT, handlerSignals) == SIG_ERR) {
+        printf("[SIGINT] : ERROR\n");
+        return 0;
+    }
+
+    if(signal(SIGQUIT, handlerSignals) == SIG_ERR) {
+        printf("[SIGQUIT] : ERROR\n");
+        return 0;
+    }
+
+    while(1) {
+        pause();
+    }
+
+    return 0;
+}
+
+void handlerSignals(int sig)
+{
+    if(sig == SIGINT) {
+        printf("\nSIGINT signal: %d\n", sig);
+        signal(SIGINT, SIG_DFL);
+    }
+
+    else if(sig == SIGQUIT) {
+        printf("\nSIGQUIT signal: %d\n", sig);
+        signal(SIGQUIT, SIG_DFL);
+    }
+}
